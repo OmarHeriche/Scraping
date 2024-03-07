c5799ac1d3e846f3df173b4fd30cc1087bb482d3
omar <herricheomar@gmail.com>
2024-03-07 12:02:01 +0100

    dynamic queries

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const puppeteer = require("puppeteer");
-const url= "https://www.jumia.com.dz/catalog/?q=ball";
-
-async function scrape(url) {
+let url= "https://www.jumia.com.dz/catalog/?q=";
+function initialzeQueries(urlWithoutQueries,SearchItem) {
+  const finalQeury=SearchItem.split(" ").join("+");
+  url=urlWithoutQueries+finalQeury;
+  return url;
+}
+url=initialzeQueries(url,/*put here what you wanna search about:*/"iphone 12");
+async function scrape(url,output) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle2' }); // wait until page load
@@ -9,20 +14,22 @@ async function scrape(url) {
   const allArticles = await page.evaluate(() => {
     const articles = document.querySelectorAll("article.prd._fb.col.c-prd");
     return Array.from(articles).map((article) => {
-      const title = article.querySelector("h3.name")?.innerText;
-      const price = article.querySelector("div.prc")?.innerText;
-      const oldPrice = article.querySelector("div.s-prc-w .old")?.innerText;
+      const title = article.querySelector("div.info h3.name")?.innerText;
+      const productHtml=article.innerHTML;
+      const curretPrice = article.querySelector("div.info div.prc")?.innerText;
+      const oldPrice = article.querySelector("div.info div.s-prc-w .old")?.innerText;
       const discount = article.querySelector("div.s-prc-w .bdg._dsct._sm")?.innerText;
       const rating = article.querySelector("div.rev .stars._s")?.innerText;
       const image = article.querySelector("div.img-c img")?.dataset.src;
       const link = article.querySelector("a.core")?.href;
-      return { title, price, oldPrice, discount, rating, image, link };
+      return { title, curretPrice, oldPrice, discount, rating, image, link, productHtml };
     });
   });
 
   console.log(allArticles);
-
   await browser.close();
+  return allArticles;
+
 }
 
 scrape(url);
